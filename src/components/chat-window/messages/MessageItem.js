7b5cd516a3d056a0f8cda2002e40d786448aa49c
Rokas <rookas.rudzenskas@gmail.com>
2021-04-21 08:34:01 +0300

    Rokas

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -9,7 +9,7 @@ import {auth} from "../../../misc/firebase";
 import {useHover, useMediaQuery} from "../../../misc/custom-hooks";
 import IconBtnControl from "./IconBtnControl";
 
-const MessageItem = ({message, handleAdmin, handleLike}) => {
+const MessageItem = ({message, handleAdmin, handleLike, handleDelete}) => {
 
     const {author, createdAt, text, likes, likeCount} = message;
     const [selfRef, isHovered] = useHover();
@@ -57,6 +57,12 @@ const MessageItem = ({message, handleAdmin, handleLike}) => {
                     tooltip="like this message"
                     onClick={() => handleLike(message.id)}
                     badgeContent={likeCount}/>
+
+                <IconBtnControl
+                    isVisible = {canShowIcons}
+                    iconName="close"
+                    tooltip="Delete this message"
+                    onClick={() => handleDelete(message.id)}/>
             </div>
 
             <div>
--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -80,13 +80,47 @@ const Messages = () => {
         });
 
         Alert.info(alertMsg, 4000);
-    }, [])
+    }, []);
+
+    const handleDelete = useCallback(
+        async (msgId, file) => {
+            // eslint-disable-next-line no-alert
+            if (!window.confirm('Delete this message?')) {
+                return;
+            }
+
+            const isLast = messages[messages.length - 1].id === msgId;
+
+            const updates = {};
+
+            updates[`/messages/${msgId}`] = null;
+
+            if (isLast && messages.length > 1) {
+                updates[`/rooms/${chatId}/lastMessage`] = {
+                    ...messages[messages.length - 2],
+                    msgId: messages[messages.length - 2].id,
+                };
+            }
+
+            if (isLast && messages.length === 1) {
+                updates[`/rooms/${chatId}/lastMessage`] = null;
+            }
+
+            try {
+                await database.ref().update(updates);
+
+                Alert.info('Message has been deleted');
+            } catch (err) {
+                return Alert.error(err.message);
+            }
+
+    }, [chatId, messages]);
 
 
     return (
         <ul className="msg-list custom-scroll">
             {isChatEmpty && <li>No messages yet</li>}
-            {canShowMessages && messages.map(msg => <MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin} handleLike={handleLikes}/>)}
+            {canShowMessages && messages.map(msg => <MessageItem key={msg.id} message={msg} handleAdmin={handleAdmin} handleLike={handleLikes} handleDelete={handleDelete}/>)}
         </ul>
     );
 };
