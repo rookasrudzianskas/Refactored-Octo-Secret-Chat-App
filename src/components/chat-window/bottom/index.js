d36b4d241435f0cf902bd7a8e8e87c36f432c2cf
Rokas <rookas.rudzenskas@gmail.com>
2021-04-22 06:23:14 +0300

    Rokas

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -68,12 +68,48 @@ const ChatBottom = () => {
             ev.preventDefault();
             onSendClick();
         }
-    }
+    };
+
+    const afterUpload = useCallback(async (files) => {
+
+        setIsLoading(true);
+
+        const updates = {}
+        files.forEach(file => {
+
+            const msgData = assembleMessage(profile, chatId);
+            msgData.file = file;
+
+            const messageId = database.ref('messages').push().key;
+
+            updates[`/messages/${messageId}`] = msgData;
+
+        })
+
+        const lastMsgId = Object.keys(updates).pop();
+
+        updates[`/rooms/${chatId}/lastMessage`] = {
+            ...updates[lastMsgId],
+            msgId: lastMsgId,
+        };
+
+
+        try {
+            await database.ref().update(updates);
+            setInput('');
+            setIsLoading(false)
+
+        } catch (err) {
+            setIsLoading(false);
+            Alert.error(err.message);
+        }
+
+    }, [chatId, profile])
 
     return (
         <div>
             <InputGroup>
-                <AttachmentBtnModal/>
+                <AttachmentBtnModal afterUpload={afterUpload}/>
                 <Input placeholder="Write a new  message here...." value={input} onChange={onInputChange} onKeyDown={onKeyDown}/>
 
                 <InputGroup.Button color="blue" appearance="primary" onClick={onSendClick} disabled={isLoading}>
