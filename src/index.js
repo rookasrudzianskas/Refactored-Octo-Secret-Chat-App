4af179e2e6bac4f8ad4aae22d877560f6d1e4672
Rokas <rookas.rudzenskas@gmail.com>
2021-04-23 07:13:30 +0300

    Rokas

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter} from "react-router-dom";
 // import reportWebVitals from './reportWebVitals';
-// import * as serviceWorker from "./serviceWorker"
+
 ReactDOM.render(
     <BrowserRouter>
         <App />
@@ -11,7 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+
