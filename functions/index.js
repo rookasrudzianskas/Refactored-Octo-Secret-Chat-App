b5f0acf748fad5ce681f5da1a5a2045c23f1ca65
Rokas <rookas.rudzenskas@gmail.com>
2021-04-23 07:21:56 +0300

    Rokas

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,14 @@
 const functions = require("firebase-functions");
 
+const admin = require("firebase-admin");
+
+const serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://glowing-computer-chat-app-default-rtdb.firebaseio.com"
+});
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
